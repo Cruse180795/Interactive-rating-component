79300bb3d8565cfdba15afd018f5bc4729e92614
ryan <ryancruse1807@gmail.com>
2022-10-05 18:33:09 +0100

    Media queries add, project completed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const submit = document.querySelector('#btn-submit');
-const card_front = document.querySelector('.card-front');
-const card_back = document.querySelector('.card-back');
+const cardFront = document.querySelector('.card-front');
+const cardBack = document.querySelector('.card-back');
 const rating = document.querySelector('#rating');
-const rate_btns = document.querySelectorAll('.rate-btn');
+const rateBtns = document.querySelectorAll('.rate-btn');
 
 // Handle submit button 
 submit.addEventListener('click', function(){
-    card_front.style.display = "none";
-    card_back.style.display = "block";
+    cardFront.style.display = "none";
+    cardBack.style.display = "block";
 });
 
 // Handle updating the score based on what the user selected
-rate_btns.forEach((btn) =>{
+rateBtns.forEach((btn) =>{
     btn.addEventListener('click', function(){
         const user_rate = btn.innerHTML;
         rating.innerHTML = `You selected ${user_rate} out of 5`;
